2270fcf8d3cba53f26ea20f1a5c00eb8b9592f54
shofiq <shofiqebr@gmail.com>
2023-11-09 15:53:38 +0600

    added delete option

--- index.js
+++ index.js
@@ -5,15 +5,7 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors(
-//   {
-//   origin:[
-//     'https://carpooling-ride-sharing-client.web.app',
-//     'https://carpooling-ride-sharing-client.firebaseapp.com/'
-//   ],
-//   credentials:true
-// }
-));
+app.use(cors());
 app.use(express.json());
 
 
@@ -36,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+  
 
     const serviceCollection =client.db('rideService').collection('services');
 
@@ -47,7 +39,7 @@ async function run() {
     })
 
 
-     app.get('/allservices',async(req,res)=>{
+     app.get('/services',async(req,res)=>{
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result); 
@@ -58,13 +50,25 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const result = await serviceCollection.findOne(query);
       res.send(result);
-  })
+  });
 
+  app.post('/services', async (req, res) => {
+    const service = req.body;
+    console.log(service);
+    const result = await serviceCollection.insertOne(service);
+    res.send(result);
+})
 
+  app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+   
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
